2218e375233ad4e7b62514470559a17c017f52a9
erwin-io <erwinramirez220@gmail.com>
2024-04-10 16:55:47 +0800

    one signal

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiBaseUrl: "https://cemify-api.vercel.app/api/v1",
-  oneSignalAppId: "2d60a699-ef0c-4a17-9786-f72b27f39eda",
+  oneSignalAppId: "57cd7844-8ada-4060-ae0d-bba1ec27ad12",
   pusher: {
     key: "1ad4b93854243ae307e6",
     cluster: "ap1",
